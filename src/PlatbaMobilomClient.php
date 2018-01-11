d0b20a4aefae5d466c8a8520fb172069c637c800
Martin Vondrák <martinvondrak@icloud.com>
2018-01-11 09:48:01 +0100

    Add response handling

--- src/PlatbaMobilomClient.php
+++ src/PlatbaMobilomClient.php
@@ -2,17 +2,31 @@
 
 namespace MartinVondrak\PlatbaMobilom;
 
+use MartinVondrak\PlatbaMobilom\Exception\InvalidSignatureException;
 use MartinVondrak\PlatbaMobilom\Http\Request;
+use MartinVondrak\PlatbaMobilom\Http\Response;
 
 class PlatbaMobilomClient implements PlatbaMobilomClientInterface
 {
     const GATEWAY = 'https://pay.platbamobilom.sk/pay/';
     const TEST_GATEWAY = 'https://pay.platbamobilom.sk/test/';
 
+    /** @var string $pwd secure key */
+    private $pwd;
+
+
     /**
      * @inheritdoc
      */
-    public function getGatewayUrl(Request $request, string $pwd, bool $debug = false): string
+    public function __construct(string $pwd)
+    {
+        $this->pwd = $pwd;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getGatewayUrl(Request $request, bool $debug = false): string
     {
         if ($debug) {
             $host = static::TEST_GATEWAY;
@@ -27,9 +41,21 @@ class PlatbaMobilomClient implements PlatbaMobilomClientInterface
             'PRICE' => $request->getPrice(),
             'URL' => $request->getUrl(),
             'EMAIL' => $request->getEmail(),
-            'SIGN' => $request->calculateSign($pwd)
+            'SIGN' => $request->calculateSign($this->pwd)
         ]);
 
         return sprintf('%s?%s', $host, $queryString);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function checkResponse(Response $response)
+    {
+        if (!$response->verifySignature($this->pwd)) {
+            throw new InvalidSignatureException();
+        }
+
+        return $response->isSuccessful();
     }
 }
--- src/PlatbaMobilomClientInterface.php
+++ src/PlatbaMobilomClientInterface.php
@@ -2,17 +2,32 @@
 
 namespace MartinVondrak\PlatbaMobilom;
 
+use MartinVondrak\PlatbaMobilom\Exception\InvalidSignatureException;
 use MartinVondrak\PlatbaMobilom\Http\Request;
+use MartinVondrak\PlatbaMobilom\Http\Response;
 
 interface PlatbaMobilomClientInterface
 {
     /**
+     * PlatbaMobilomClientInterface constructor
+     *
+     * @param string $pwd secret key
+     */
+    public function __construct(string $pwd);
+
+    /**
      * Prepare data and generate URL for redirect to gateway
      *
      * @param Request $request data for payment request
-     * @param string  $pwd secure key
      * @param bool    $debug flag whether test gateway should be used
      * @return string gateway URL with parameters
      */
-    public function getGatewayUrl(Request $request, string $pwd, bool $debug = false);
+    public function getGatewayUrl(Request $request, bool $debug = false);
+
+    /**
+     * @param Response $response
+     * @return mixed
+     * @throws InvalidSignatureException
+     */
+    public function checkResponse(Response $response);
 }
